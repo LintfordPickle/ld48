22b76405a194924cb1e57d94c6db514fdccbc154
John Hampson <jonnohampson@gmail.com>
2021-04-24 09:43:13 +0200

    added GameScreen

--- LDTest/src/net/ruse/ld48/BaseGame.java
+++ LDTest/src/net/ruse/ld48/BaseGame.java
@@ -1,9 +1,33 @@
 package net.ruse.ld48;
 
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+
+import org.lwjgl.opengl.GL11;
+
 import net.lintford.library.GameInfo;
 import net.lintford.library.core.LintfordCore;
+import net.lintford.library.core.debug.Debug;
+import net.lintford.library.core.graphics.ColorConstants;
+import net.lintford.library.core.graphics.textures.Texture;
+import net.lintford.library.core.graphics.textures.texturebatch.TextureBatchPCT;
+import net.lintford.library.screenmanager.ScreenManager;
+import net.ruse.ld48.screens.GameScreen;
 
 public class BaseGame extends LintfordCore {
+
+	// ---------------------------------------------
+	// Variables
+	// ---------------------------------------------
+
+	private ScreenManager mScreenManager;
+
+	// ---------------------------------------------
+	// Properties
+	// ---------------------------------------------
+
+	public ScreenManager screenManager() {
+		return mScreenManager;
+	}
 
 	// ---------------------------------------------
 	// Constructor
@@ -12,10 +36,94 @@ public class BaseGame extends LintfordCore {
 	public BaseGame(GameInfo pGameInfo, String[] pArgs) {
 		super(pGameInfo, pArgs, false);
 
+		mIsFixedTimeStep = true;
+
+		mScreenManager = new ScreenManager(this);
+
+		Debug.debugManager().logger().mirrorLogToConsole(true);
+
 	}
 
 	// ---------------------------------------------
-	// Entry
+	// Core-Methods
+	// ---------------------------------------------
+
+	@Override
+	protected void showStartUpLogo(long pWindowHandle) {
+		// Show a mini-splash screen
+		Texture lTexture = mResourceManager.textureManager().loadTexture("TEXTURE_SPLASH", "res/textures/textureSplash.png", LintfordCore.CORE_ENTITY_GROUP_ID);
+
+		GL11.glClearColor(0f, 0f, 0f, 1f);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+		TextureBatchPCT lTB = new TextureBatchPCT();
+		lTB.loadGLContent(mResourceManager);
+		lTB.begin(mHUD);
+		lTB.draw(lTexture, 0, 0, 640, 480, -320, -240, 640, 480, -0.1f, ColorConstants.WHITE);
+		lTB.end();
+
+		glfwSwapBuffers(pWindowHandle);
+
+	}
+
+	@Override
+	protected void onInitializeApp() {
+		super.onInitializeApp();
+
+		mScreenManager.addScreen(new GameScreen(mScreenManager));
+
+		mScreenManager.initialize();
+
+	}
+
+	@Override
+	protected void oninitializeGL() {
+		super.oninitializeGL();
+
+	}
+
+	@Override
+	protected void onLoadGLContent() {
+		super.onLoadGLContent();
+
+		mScreenManager.loadGLContent(mResourceManager);
+
+	}
+
+	@Override
+	protected void onUnloadGLContent() {
+		super.onUnloadGLContent();
+
+		mScreenManager.unloadGLContent();
+
+	}
+
+	@Override
+	protected void onHandleInput() {
+		super.onHandleInput();
+
+		mScreenManager.handleInput(this);
+
+	}
+
+	@Override
+	protected void onUpdate() {
+		super.onUpdate();
+
+		mScreenManager.update(this);
+
+	}
+
+	@Override
+	protected void onDraw() {
+		super.onDraw();
+
+		mScreenManager.draw(this);
+
+	}
+
+	// ---------------------------------------------
+	// Entry Point
 	// ---------------------------------------------
 
 	public static void main(String[] pArgs) {
---
+++ LDTest/src/net/ruse/ld48/screens/GameScreen.java
@@ -0,0 +1,63 @@
+package net.ruse.ld48.screens;
+
+import net.lintford.library.core.LintfordCore;
+import net.lintford.library.core.ResourceManager;
+import net.lintford.library.screenmanager.ScreenManager;
+import net.lintford.library.screenmanager.screens.BaseGameScreen;
+
+public class GameScreen extends BaseGameScreen {
+
+	// ---------------------------------------------
+	// Variables
+	// ---------------------------------------------
+
+	// ---------------------------------------------
+	// Constructor
+	// ---------------------------------------------
+
+	public GameScreen(ScreenManager pScreenManager) {
+		super(pScreenManager);
+
+	}
+
+	// ---------------------------------------------
+	// Core-Methods
+	// ---------------------------------------------
+
+	@Override
+	public void initialize() {
+		super.initialize();
+
+	}
+
+	@Override
+	public void loadGLContent(ResourceManager pResourceManager) {
+		super.loadGLContent(pResourceManager);
+
+	}
+
+	@Override
+	public void unloadGLContent() {
+		super.unloadGLContent();
+
+	}
+
+	@Override
+	public void handleInput(LintfordCore pCore) {
+		super.handleInput(pCore);
+
+	}
+
+	@Override
+	public void update(LintfordCore pCore, boolean pOtherScreenHasFocus, boolean pCoveredByOtherScreen) {
+		super.update(pCore, pOtherScreenHasFocus, pCoveredByOtherScreen);
+
+	}
+
+	@Override
+	public void draw(LintfordCore pCore) {
+		super.draw(pCore);
+
+	}
+
+}
