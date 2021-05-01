2175f82ca72e55a149399078bf95844ff6ec927e
John Hampson <jonnohampson@gmail.com>
2021-05-01 14:15:24 +0200

    remove LintfordLib from reposity (submodule)

--- LintfordLib/src/net/lintford/library/controllers/messages/IMessageController.java
+++
@@ -1,9 +0,0 @@
-package net.lintford.library.controllers.messages;
-
-public interface IMessageController {
-
-	public void addMessage(String pTag, String pMessage);
-
-	public void addMessage(String pTag, String pMessage, int pCharacterWrapAmount);
-
-}
--- LintfordLib/src/net/lintford/library/core/EntityGroupManager.java
+++
@@ -1,9 +0,0 @@
-package net.lintford.library.core;
-
-public abstract class EntityGroupManager {
-
-	public abstract int increaseReferenceCounts(int pEntityGroupID);
-
-	public abstract int decreaseReferenceCounts(int pEntityGroupID);
-
-}
